b686037c3590e44e8a236d424efea034d718f576
chendo <chendo at chendo dot net>
2009-06-14 01:16:14 +1000

    made the time rain easier to read

--- examples/time.rb
+++ examples/time.rb
@@ -1,12 +1,17 @@
 $:.unshift(File.dirname(__FILE__) + "/..") 
 require 'lib/neo'
 
+# Time encoded in matrix time
+# 24 hour time
+# First digit in the first quarter starting from the left, second digit in the second quarter, etc
+# Count the number of highlighted glyphs in each trail to get the digit
+
 Neo.init
 Neo.max_trails = 60
 Neo.spawn do
   code = Time.now.strftime("%H%M").split(//).map { |i| i.to_i }
   index = rand(4)
-  pattern = [true, [false] * code[index], true].flatten
+  pattern = [true] * code[index]
   x = index * width / 4 + rand(width / 4)
   Trail.new(self, :pattern => pattern, :length => pattern.length + rand(3), :x => x, :speed => 2..7)
 end
